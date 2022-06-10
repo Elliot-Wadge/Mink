b7f8844c1b37fdf5078ea6079c5a6d8f24b8597b
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2022-06-10 00:24:05 -0700

    setting up necessary files for package

---
+++ __setup__.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='Mink',
+    version='0.0.1',
+    author='Elliot Wadge',
+    author_email='ewadge@sfu.ca',
+    description='Package containing useful functions for scientific analysis',
+    url='https://github.com/Elliot-Wadge/Mink',
+    license='MIT',
+    packages=['Mink'],
+    install_requires=['numpy','plotly','scipy'],
+)
