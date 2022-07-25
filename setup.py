daa3ef777b36d4a71dbc3690087312819ae5e086
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2022-07-24 23:17:37 -0700

    update the version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setuptools.setup(
     name='Mink',
-    version='0.0.8',
+    version='0.0.9',
     author='Elliot Wadge',
     author_email='ewadge@sfu.ca',
     description='Package containing useful functions for scientific analysis',
     url='https://github.com/Elliot-Wadge/Mink',
     license='MIT',
     packages=["Mink"],
-    install_requires=['numpy', 'plotly', 'scipy'],
+    install_requires=['numpy', 'scipy'],
 )
