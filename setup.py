3d10361611a62ebcbf65e1aa3102d10576ffeba6
Elliot <ewadge@sfu.ca>
2022-11-02 15:28:19 -0700

    changes to sc notation and minor changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='Mink',
-    version='0.1.2',
+    version='0.2.2',
     author='Elliot Wadge',
     author_email='ewadge@sfu.ca',
     description='Package containing useful functions for scientific analysis',
