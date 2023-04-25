73363f9d7ed056b67e7109985c27f683374029c3
Elliot <ewadge@sfu.ca>
2023-04-25 12:39:08 -0700

    added a plotly template for double column formatting

--- Mink/plotly_templates.py
+++ Mink/plotly_templates.py
@@ -26,3 +26,29 @@ science_template.layout['height'] = 500
 science_template.layout['autosize'] = False
 science_template.layout["font_size"] = 19
 science_template.layout['font_family'] = 'Times New Roman'
+
+
+dc_template = go.layout.Template()
+dc_lst = []
+dcgl_lst = []
+colors = ["rgb(20,20,20)", "rgb(169,2,235)", "rgb(6,96,214)", "rgb(20,156,5)",
+          "rgb(242,135,5)", "rgb(58,184,189)", "rgb(211,148,242)"]
+
+for color in colors:
+    dc_lst.append(go.Scatter(marker=dict(color=color),
+                             line=dict(color=color),
+                             hovertemplate="x = %{x}<br>y = %{y}"))
+    dcgl_lst.append(go.Scattergl(marker=dict(color=color),
+                               line=dict(color=color),
+                               hovertemplate="x = %{x}<br>y = %{y}"))
+
+dc_template.data.scatter = dc_lst
+dc_template.data.scattergl = dcgl_lst
+dc_template.layout = pio.templates["simple_white"].layout
+dc_template.layout["margin"] = go.layout.Margin(autoexpand=True,
+                                                l=30, r=30, t=30, b=30)
+dc_template.layout['width'] = 500
+dc_template.layout['height'] = 500
+dc_template.layout['autosize'] = False
+dc_template.layout["font_size"] = 22
+dc_template.layout['font_family'] = 'Times New Roman'
--- test/test_templates.py
+++ test/test_templates.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from plotly import graph_objects as go
-from Mink.plotly_templates import science_template
+from Mink.plotly_templates import science_template, dc_template
 from Mink.special_chars_dict import sc
 
 class TemplateTester(unittest.TestCase):
@@ -15,6 +15,17 @@ class TemplateTester(unittest.TestCase):
         fig.add_trace(go.Scatter(x=self.x, y=2*self.x))
         fig.update_xaxes(title=f"this is the x title {sc.pi + sc.psi + sc.chi}")
         fig.show()
+
+    def test_dc_template(self):
+        '''check that the template doesn't produce any errors'''
+        fig = go.Figure()
+        fig.update_layout(template=dc_template)
+        fig.add_trace(go.Scatter(x=self.x, y=self.x))
+        fig.add_trace(go.Scatter(x=self.x, y=2*self.x))
+        fig.update_xaxes(title=f"this is the x title {sc.pi + sc.psi + sc.chi}")
+        fig.show()
+        
+        
 
 
 if __name__ == '__main__':
